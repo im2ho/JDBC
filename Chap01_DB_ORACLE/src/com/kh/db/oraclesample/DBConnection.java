4e8a8f1e2e91f94a0637056e09d8b7ce6c98697a
im2ho <84431276+im2ho@users.noreply.github.com>
2023-10-17 17:37:36 +0900

    connection

--- Chap01_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
+++ Chap01_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
@@ -10,19 +10,25 @@ import java.sql.Date;
 public class DBConnection {
 
 	public static void main(String[] args) {
+		//DBConnection d = new DBConnection();
+		//d.SelectKHCAFE();
+		intsertBank();
+	} //main()
+	
+	static void SelectBank() {
 		//1. 드라이버 연결 : Oracle JDBC 드라이버 클래스 이름
-		String driver = "oracle.jdbc.driver.OracleDriver";
+		//String driver = "oracle.jdbc.driver.OracleDriver";
 		//2. ORACLE 내 컴퓨터 연결 : 데이터 베이스 연결 정보
 									  //나의IP주소:port번호:SID
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe"; //thin : 100% 자바 클라이언트 드라이버
 		String user = "khbank";
 		String password = "khbank";
 		Connection con = null;
 		try { //100% DB연결 보장 불가능하기 때문에 예외처리 필수
 			//연결을 사용하여 쿼리 실행 또는 데이터베이스 작업 수행
 			con = DriverManager.getConnection(url, user, password);
-			System.out.println("데이터베이스 연결 완료\n");
-			
+			System.out.println("데이터베이스 연결 완료\n"); 
+					
 			//SELECT 쿼리 예제
 			String selectQuery = "SELECT * FROM bank";
 			PreparedStatement selectState = con.prepareStatement(selectQuery);
@@ -39,10 +45,150 @@ public class DBConnection {
 				System.out.println("ACCOUNT_ID : " + accountID + ", ACCOUNT_NAME : " + accountName + ", Balance : " + balance);
 				System.out.println("ACCOUNT_NUMBER : " + accountNumber + ", BRANCHNAME : " + branchName);
 				System.out.println("LAST_TRANSACTION_DATE : " + lastTransactionDate + "\n");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}		
+	} //SelectBank()
+	
+	static void SelectKHCAFE() {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "khcafe";
+		String password = "khcafe";
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("데이터베이스 연결 완료 ~.~\n");
+			
+			String selectQuery = "SELECT menu_id, mname, price, description FROM menu WHERE price > 10"; //menu 테이블 전체 선택
+			PreparedStatement selectState = con.prepareStatement(selectQuery);
+			ResultSet result = selectState.executeQuery();
+			while(result.next()) {
+				//int cafeID = result.getInt("cafe_id");
+				int menuID = result.getInt("menu_id");
+				String menuName = result.getString("mname");
+				double price = result.getDouble("price");
+				String description = result.getString("description");
+				
+				//System.out.println("CAFE_ID : " + cafeID);
+				System.out.println("MENU_ID : " + menuID + ", MENU_NAME : " + menuName + ", PRICE : " + price);
+				System.out.println("MENU_DESCRIPTION : " + description +"\n");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	} //SelectKHCAFE()
+	
+	static void SelectWhile() {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "khbank";
+		String password = "khbank";
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			
+			String selectQuery = "SELECT * FROM BANK WHERE ACCOUNT_NUMBER IN(?,?)" ; //미지의 수
+			PreparedStatement selectState = con.prepareStatement(selectQuery);
+			
+			String[] targetAN = {"333302123542","644644644644"};
+			selectState.setString(1,targetAN[0]); //targetAN[0]: 1번째 ?에 들어갈 값
+			selectState.setString(2,targetAN[1]); //targetAN[0]: 2번째 ?에 들어갈 값
+			ResultSet result = selectState.executeQuery();
+			
+			//값의 존재 여부 판단
+			if(!result.isBeforeFirst()) { //데이터가 없을 경우를 나타내기 위해 조건을 false로 설정
+				System.out.println("존재하는 데이터가 없습니다.");
+			}
+			while(result.next()) {
+				int aid = result.getInt("account_id");
+				String anum = result.getString("account_number");
+				String aname = result.getString("account_name");
+				int balance = result.getInt("balance");
+				String bname = result.getString("branch_name");
+				Date ltd = result.getDate("last_transaction_date");
+				
+				System.out.println("Account_ID : " + aid + ", ACCOUNT_NAME : " + aname);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	static void SelectIf() {
+		String driver = "oracle.jdbc.driver.OracleDriver";
 
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "khbank";
+		String password = "khbank";
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("ORACLE DB 연결 완료\n");
+			
+			//WHERE절 사용하여 조건 추가
+			String selectQuery = "SELECT * FROM BANK WHERE balance=? AND account_number=?"; //미지의 수
+			PreparedStatement selectState = con.prepareStatement(selectQuery);
+			
+			
+			//원하는 조건의 account_id 설정
+			int targetBalance = 1500;
+	
+			//조건 설정
+			selectState.setInt(1,targetBalance); //targetBalance: 1번째 ?에 들어갈 값
+			selectState.setString(2,"1002159226496"); //"1002159226496" : 2번째 ?에 들어갈 값
+			ResultSet result = selectState.executeQuery();
+			
+			//while문과 달리, 조건에 부합하는 데이터가 없을 때 문구 출력이 가능하다
+			if(result.next()) {
+				int aid = result.getInt("account_id");
+				String anum = result.getString("account_number");
+				String aname = result.getString("account_name");
+				int balance = result.getInt("balance");
+				String bname = result.getString("branch_name");
+				Date ltd = result.getDate("last_transaction_date");
+				
+				System.out.println("Account_ID : " + aid + ", ACCOUNT_NAME : " + aname);
+			} else {
+				System.out.println("조건에 해당하는 데이터가 없습니다;;");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	static void intsertBank() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe"; //thin : 100% 자바 클라이언트 드라이버
+		String user = "khbank";
+		String password = "khbank";
+		//Connection con = null; //Connection : java.sql 기본 제공
+		
+		try {
+			Connection con = DriverManager.getConnection(url, user, password);
+			String insertQuery = 
+					"INSERT INTO bank (account_id, account_number, account_name, balance, branch_name, last_transaction_date)"
+					+ "VALUES(?,?,?,?,?,?)";
+			
+			PreparedStatement insertState = con.prepareStatement(insertQuery);
+			
+			insertState.setInt(1, 100);
+			insertState.setString(2, "333302578912");
+			insertState.setString(3, "호랑이");
+			insertState.setDouble(4, 500);
+			insertState.setString(5, "역삼");
+			insertState.setDate(6, Date.valueOf("2023-10-17"));
+			
+			int rowsInsert = insertState.executeUpdate();
+			System.out.println(rowsInsert + "row 추가 완료");
+			
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ Chap01_DB_ORACLE/src/com/kh/db/oraclesample/SELECTexercise.java
@@ -0,0 +1,49 @@
+package com.kh.db.oraclesample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SELECTexercise {
+
+	public static void main(String[] args) {
+		select_ex();
+	}
+	
+	public static void select_ex() {
+		//1. 드라이버 연결
+		//String driver = "oracle.jdbc.driver.OracleDriver";
+		//2. ORACLE과 컴퓨터 연결 : DB 연결 정보
+		String url = "jdbc:oracle:thin:@localhost:1521:xe"; //thin : 100% 자바 클라이언트 드라이버
+		String user = "say";
+		String password = "say";
+		Connection con = null; //Connection : java.sql 기본 제공
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("ORACLE DB 연결 완료\n");
+			
+			//SELECT 쿼리 예제
+			String selectQuery = 
+					"SELECT * FROM nationalpokedex WHERE type=? AND base_stats=?";
+			PreparedStatement selectState = con.prepareStatement(selectQuery);
+			
+			
+			selectState.setString(1,"드래곤");
+			selectState.setInt(2, 600);		
+			
+			ResultSet result = selectState.executeQuery();
+			
+			while(result.next()) {
+				String name = result.getString("name");
+				int bs = result.getInt("base_stats");
+				System.out.println("이름: " + name + " / 종족치: " + bs);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
